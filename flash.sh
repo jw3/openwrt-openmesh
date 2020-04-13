e4cda9aa9f81b5a46b9de4e39d0a63c98bbf8da5
John Wass <wassj@ctc.com>
2020-04-13 11:17:01 -0400

    lede updates

--- flash.sh
+++ flash.sh
@@ -3,4 +3,4 @@
 readonly dev="${1?usage:flash.sh <eth-device> [tag]}"
 readonly tag="${2:-latest}"
 
-docker run --rm -it --privileged --network=host openwrt-openmesh ap51-flash "$dev" openwrt-ar71xx-generic-om2p-squashfs-factory.bin
+docker run --rm -it --privileged --network=host openwrt-openmesh ap51-flash "$dev" lede-ar71xx-generic-om2p-squashfs-factory.bin
